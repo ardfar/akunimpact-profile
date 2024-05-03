ac1886c5b7d571a460bc1833faabc79ae958b161
Daniel Mw <danielmw101@gmail.com>
2024-05-03 19:31:52 +0700

    card revision and creating the light cone page

--- app.py
+++ app.py
@@ -2,17 +2,20 @@ import os
 import glob
 
 # Path to the directory containing the images
-directory = r'C:\Users\Daniel Maheswara\Desktop\akunimpact-profile-dev\assets\images\games\genshin\weapons'
+directory = r'C:\Users\Daniel Maheswara\Desktop\akunimpact-profile-dev\assets\images\games\hsr\light-cones'
 
-# Iterate over each folder
-for folder in os.listdir(directory):
-    folder_path = os.path.join(directory, folder)
-    
-    # Check if the item in the directory is a folder
-    if os.path.isdir(folder_path):
-        # Use glob to find all .webp files in the folder
-        image_files = glob.glob(os.path.join(folder_path, '*.webp'))
-        
-        # Print the names of the image files
-        for image_file in image_files:
-            print(os.path.basename(image_file))
+# Get a list of all image files in the directory
+image_files = glob.glob(os.path.join(directory, '*.webp'))
+
+# Create a dictionary to store the file names and character names
+image_dict = {}
+
+# Populate the dictionary with file names and corresponding character names
+for image_file in image_files:
+    file_name = os.path.basename(image_file)
+    character_name = file_name.split('.')[0].replace('-', ' ').title()
+    image_dict[file_name] = character_name
+
+# Print the dictionary
+for file_name, character_name in image_dict.items():
+    print(f"'{file_name}': '{character_name}'")
